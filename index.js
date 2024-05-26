f10eb3fef36594c3f941c5ad75bdfc5d186a2516
Vitor <schmidt_vitor@hotmail.com>
2024-05-25 21:54:13 -0300

    deixando online o ome

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+import { Client, GatewayIntentBits } from "discord.js";
+
 console.log("aham");
+
+const client = new Client({
+  intents: [GatewayIntentBits.GuildMessages, GatewayIntentBits.Guilds],
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on("ready", () => {
+  console.log("O pai ta ON (online)");
+});
